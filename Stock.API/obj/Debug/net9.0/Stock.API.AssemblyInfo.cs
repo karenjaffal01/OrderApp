e034ebf517cbdc6cfbe669d50c9394060cb77759
Karen Jaffal <karen.jaffal@montyholding.com>
2025-08-20 15:32:33 +0300

    Configured Ocelot

--- Stock.API/obj/Debug/net9.0/Stock.API.AssemblyInfo.cs
+++ Stock.API/obj/Debug/net9.0/Stock.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfde35abef1ace8b0b2d83ea37b412d3802b7ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6d92be46dc6edd3ac38e8f590b2c4e941c49e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
